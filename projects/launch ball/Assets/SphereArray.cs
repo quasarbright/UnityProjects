8435dee43b50c086474eb2a8cd11ba696c6b6ec7
quasarbright <mthundershock@gmail.com>
2019-05-10 01:53:16 -0400

    shoot balls at cubes

---
+++ projects/launch ball/Assets/SphereArray.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereArray : MonoBehaviour
+{
+    public Rigidbody spherePrefab;
+    public float speed = 100F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Spawn() {
+        Rigidbody newSphere = Instantiate(spherePrefab, transform.position + transform.forward, transform.rotation);
+        newSphere.AddForce(transform.forward * speed, ForceMode.VelocityChange);
+        // spheres.Add(newSphere);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Spawn();
+        }
+    }
+}
---
+++ projects/launch ball/Assets/move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed = 5;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if(Input.GetKey("w")) {
+            controller.SimpleMove(controller.transform.forward * speed);
+        }
+        if(Input.GetKey("a")) {
+            controller.SimpleMove(controller.transform.right * -speed);
+
+        }
+        else if(Input.GetKey("s")) {
+            controller.SimpleMove(controller.transform.forward * -speed);
+
+        }
+        else if(Input.GetKey("d")) {
+            controller.SimpleMove(controller.transform.right * speed);
+
+        }
+    }
+}
