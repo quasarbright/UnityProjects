322cb47dec4d89382ec82b2764449fa1b3d047fc
quasarbright <mthundershock@gmail.com>
2019-05-11 17:20:39 -0400

    score, vowel balance

--- projects/Anagrams/Assets/Scripts/LettersHandler.cs
+++ projects/Anagrams/Assets/Scripts/LettersHandler.cs
@@ -17,32 +17,63 @@ public class LettersHandler : MonoBehaviour
 
     string word = "";
 
+    public Text scoreText;
+
+    int score = 0;
+
+    HashSet<string> enteredWords = new HashSet<string>();
+
     // Start is called before the first frame update
     void Start()
     {
         // randomize characters on startup
         System.Random rand = new System.Random();
+        int count = 0;
+        // initialize usable letters randomly
         foreach (Text text in texts)
         {
-            int index = rand.Next(0, 26);
-            char letter = alphabet[index];
+            char letter;
+            // ensure 2 vowels
+            if (count < 2)
+            {
+                int index = rand.Next(0, 5);
+                letter = vowels[index];
+            }
+            else
+            {
+                int index = rand.Next(0, 21);
+                letter = consonants[index];
+            }
             usableLetters.Add(letter);
+            count++;
+        }
+        // shuffle
+        usableLetters.Sort((x, y) => rand.Next(-1,2));
+        for (int i = 0; i < texts.Count; i++)
+        {
+            Text text = texts[i];
+            char letter = usableLetters[i];
             text.text = letter.ToString();
         }
+
         // add input change listener
         input.onValueChanged.AddListener(delegate { OnChange(); });
         input.onEndEdit.AddListener(delegate { OnEnd(); });
         input.Select();
         input.ActivateInputField();
+
+        scoreText.text = score.ToString();
     }
 
-    // Update is called once per frame
     void OnChange()
     {
         string newWord = input.text;
-        if(!IsValid(newWord)){
+        if (!IsValid(newWord))
+        {
             input.text = word;
-        } else {
+        }
+        else
+        {
             input.text = newWord;
             word = newWord;
         }
@@ -50,9 +81,20 @@ public class LettersHandler : MonoBehaviour
 
     void OnEnd()
     {
+        if (!enteredWords.Contains(word))
+        {
+            score += calcScore(word);
+            scoreText.text = score.ToString();
+            enteredWords.Add(word);
+        }
         input.text = "";
         input.Select();
         input.ActivateInputField();
+    }
+
+    static int calcScore(string word)
+    {
+        return word.Length * 100;
     }
 
     // is the word valid given the list of usable letters?
