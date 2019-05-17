05bd06a577c2bcd30579dd5bf279025f5ba40995
quasarbright <mthundershock@gmail.com>
2019-05-16 23:12:31 -0400

    matrices and tests

---
+++ projects/SelfDrivingCar/Assets/Matrix.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+public class Matrix
+{
+    private float[,] vals;
+    public readonly int rows;
+    public readonly int cols;
+    public Matrix(float[,] vals)
+    {
+        this.vals = vals;
+        rows = vals.GetLength(0);
+        cols = vals.GetLength(1);
+    }
+    public Matrix(int rows, int cols)
+    {
+        vals = new float[rows, cols];
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    public float Get(int r, int c)
+    {
+        return vals[r, c]; 
+    }
+
+    public void Set(int r, int c, float val)
+    {
+        vals[r, c] = val;
+    }
+
+    public Matrix MatMul(Matrix other)
+    {
+        if(other == null)
+        {
+            throw new System.ArgumentNullException("other matrix in matmul cannot be null");
+        }
+        if(this.cols != other.rows)
+        {
+            throw new System.ArgumentException("matmul shape mismatch: ("+rows+", "+cols+") x ("+other.rows+", "+other.cols+")");
+        }
+        Matrix ans = new Matrix(this.rows, other.cols);
+        for(int ansr = 0; ansr < ans.rows; ansr++)
+        {
+            for(int ansc = 0; ansc < ans.cols; ansc++)
+            {
+                float dotProduct = 0;
+                for(int i = 0; i < this.cols; i++)
+                {
+                    // this.cols should be equal to other.rows
+                    dotProduct += this.Get(ansr, i) * other.Get(i, ansc);
+                }
+                ans.Set(ansr, ansc, dotProduct);
+            }
+        }
+        return ans;
+    }
+}
---
+++ projects/SelfDrivingCar/Assets/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tests : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("matmul: "+TestMatMul().ToString());
+    }
+    bool TestMatMul()
+    {
+        float[,] vals1 = new float[3, 2]
+        {
+            {1, 2},
+            {3, 4},
+            {5, 6},
+        };
+        Matrix m1 = new Matrix(vals1);
+
+        float[,] vals2 = new float[2, 4]
+        {
+            {7,8,9,10},
+            {11,12,13,14}
+        };
+        Matrix m2 = new Matrix(vals2);
+        Matrix actual = m1.MatMul(m2);
+        float[,] expectedVals = new float[3, 4]
+        {
+            {29,32,35,38},
+            {65,72,79,86},
+            {101,112,123,134}
+        };
+        Matrix expected = new Matrix(expectedVals);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (expected.Get(i, j) != actual.Get(i, j))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
