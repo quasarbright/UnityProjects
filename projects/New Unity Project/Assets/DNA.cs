846e04a0adc171c0adc4e3561ee9f5a1c74e9d2a
quasarbright <mthundershock@gmail.com>
2019-08-31 15:36:50 -0400

    dna and guy

---
+++ projects/New Unity Project/Assets/DNA.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA
+{
+    public Vector3[] forces;
+
+    public int Length
+    {
+        get
+        {
+            return forces.Length;
+        }
+    }
+
+    public DNA(int length)
+    {
+        // initialize forces randomly with magnitude 1
+        this.forces = new Vector3[length];
+        for(int i = 0; i < length; i++)
+        {
+            this.forces[i] = Random.onUnitSphere;
+        }
+    }
+
+    public DNA(DNA parent1, DNA parent2)
+    {
+        // create DNA based on two parents
+        Debug.Assert(parent1.Length == parent2.Length);
+        int length = parent1.Length;
+        Vector3[] newForces = new Vector3[length];
+        for(int i = 0; i < length; i++)
+        {
+            if(Random.Range(0.0f, 1.0f) < 0.5f)
+            {
+                newForces[i] = parent1.forces[i];
+            }
+            else
+            {
+                newForces[i] = parent2.forces[i];
+            }
+        }
+        this.forces = newForces;
+    }
+
+    public void Mutate(float mutationRate)
+    {
+        // mutation rate is a probability from 0 to 1 of each gene randomly mutating
+        // this method randomly mutates this DNA's genes according to the mutation rate
+        for(int i = 0; i < this.Length; i++)
+        {
+            if(Random.Range(0f, 1f) < mutationRate)
+            {
+                // mutate this gene
+                this.forces[i] = Random.onUnitSphere;
+            }
+        }
+    }
+}
--- projects/New Unity Project/Assets/GuyBehavior.cs
+++ projects/New Unity Project/Assets/GuyBehavior.cs
@@ -2,17 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// this class is responsible for reading DNA and moving based on it
+// it knows when to die and how to calculate fitness
+// it never triggers crossover or mutation and doesn't know about other Guys
+[RequireComponent(typeof(Rigidbody))]
 public class GuyBehavior : MonoBehaviour
 {
+    // maximum age
+    public int lifespan = 100;
+    // how strongly to apply the forces
+    public float forceMagnitude = 10;
+    // age increments every tick and we die when we hit our lifespan
+    int age = 0;
+    public GameObject target;
+    // contains force sequence
+    DNA dna;
+    Rigidbody rb;
+    // should we keep moving?
+    bool dead = false;
+    // have we hit the target?
+    bool succeeded = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dna = new DNA(lifespan);
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // face the direction we're moving
+        this.transform.rotation = Quaternion.LookRotation(this.rb.velocity, Vector3.up);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(Object.ReferenceEquals(collision.gameObject, this.target))
+        {
+            // we hit the target
+            this.succeeded = true;
+            this.dead = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(!dead)
+        {
+            // apply current force and increment age
+            Vector3 force = dna.forces[this.age];
+            force = force * Time.fixedDeltaTime * 100;
+            force = force * this.forceMagnitude;
+            this.rb.AddForce(force);
+            this.age++;
+            if(this.age == this.lifespan)
+            {
+                this.dead = true;
+            }
+        }
+        else
+        {
+            //we're dead. freeze
+            this.rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+    public float CalcFitness()
+    {
+        // we want a fitness function that is inversely related to distance from target
+        // so the closer to the target, the more fitness
+        Vector3 disp = this.target.transform.position - this.transform.position;
+        return 20*20 - disp.sqrMagnitude;
+    }
+
+    public void ResetValues()
+    {
+        this.age = 0;
+        this.dead = false;
+        this.succeeded = false;
+        this.transform.position = Vector3.zero;
+        this.rb.constraints = RigidbodyConstraints.None;
+        this.rb.velocity = Vector3.zero;
+        this.transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
     }
 }
