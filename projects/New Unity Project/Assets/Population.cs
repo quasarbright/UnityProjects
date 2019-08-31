f2097ec8ac53bd81763f64b030d058d5fd31ad10
quasarbright <mthundershock@gmail.com>
2019-08-31 15:46:15 -0400

    start population

---
+++ projects/New Unity Project/Assets/Population.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Population : MonoBehaviour
+{
+    public int populationSize = 10;
+    public GameObject guyPrefab;
+    private GameObject[] guys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // instantiate guys randomly
+        this.guys = new GameObject[this.populationSize];
+        for(int i = 0; i < this.populationSize; i++)
+        {
+            this.guys[i] = Instantiate(this.guyPrefab, this.transform.position, this.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
