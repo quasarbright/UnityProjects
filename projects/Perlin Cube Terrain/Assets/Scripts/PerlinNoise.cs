8ddcad95bac507be6eed785b63d62e7ad6991202
quasarbright <mthundershock@gmail.com>
2019-09-02 16:38:17 -0400

    static terrain good

---
+++ projects/Perlin Cube Terrain/Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise
+{
+    float frequency;
+    float seed;
+
+    public PerlinNoise(float frequency)
+    {
+        this.frequency = frequency;
+        this.seed = Random.Range(-10000, 10000);
+    }
+
+    public float Sample(float x, float y)
+    {
+        return Mathf.PerlinNoise(x, y);        
+    }
+
+    public float Sample(float x, float y, float z)
+    {
+        x = x * this.frequency + seed;
+        y = y * this.frequency + seed;
+        z = z * this.frequency + seed;
+        float ans = 0f;
+        ans += this.Sample(x, y);
+        ans += this.Sample(y, x);
+        ans += this.Sample(x, z);
+        ans += this.Sample(z, x);
+        ans += this.Sample(y, z);
+        ans += this.Sample(z, y);
+        ans = ans / 6f;
+        return ans;
+    }
+}
---
+++ projects/Perlin Cube Terrain/Assets/Scripts/PerlinTerrain.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinTerrain : MonoBehaviour
+{
+    [Tooltip("The dimensions of the terrain, in number of cubes")]
+    public int length, width, height;
+
+    [Tooltip("Frequency of the noise. Higher means less smooth")]
+    public float frequency = 0.1f;
+
+    [Tooltip("Lower means less empty space")]
+    [Range(0f, 1f)]
+    public float threshold = 0.5f;
+
+    [Tooltip("The block the world will be constructed from")]
+    public GameObject blockPrefab;
+
+    GameObject[,,] blocks;
+
+    PerlinNoise noise;
+    // Start is called before the first frame update
+    void Start()
+    {
+        noise = new PerlinNoise(frequency);
+
+        blocks = new GameObject[length,width,height];
+        for (int x = 0; x < length; x++)
+        {
+            for (int z = 0; z < width; z++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Vector3 pos = new Vector3(x, y, z); // TODO scale with the scale of the prefab
+                    blocks[x, z, y] = Instantiate(blockPrefab, pos, Quaternion.LookRotation(Vector3.forward, Vector3.up));
+                    // blocks[x, z, y].SetActive(false);
+                    // float value = noise.Sample(x, y, z);
+                    // Debug.Log(value);
+                    // if (value >= threshold)
+                    // {
+                    //     GameObject block = blocks[x, y, z];
+                    //     block.SetActive(true);
+                    // }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // noise = new PerlinNoise(frequency);
+        for (int x = 0; x < length; x++)
+        {
+            for (int z = 0; z < width; z++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    float value = noise.Sample(x, y, z);
+                    // Debug.Log(value);
+                    GameObject block = blocks[x, z, y];
+                    if (value >= threshold)
+                    {
+                        block.SetActive(true);
+                    }
+                    else
+                    {
+                        block.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
