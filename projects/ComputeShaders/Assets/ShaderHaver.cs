55d65a170460e59c9032844967efb4186d00c80d
Mike Delmonaco <mthundershock@gmail.com>
2020-01-08 01:18:17 -0500

    get a basic compute shader rendering

---
+++ projects/ComputeShaders/Assets/ShaderHaver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderHaver : MonoBehaviour
+{
+    public ComputeShader shader;
+    public RenderTexture rt;
+
+    public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(SystemInfo.supportsComputeShaders);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int kernelNumber = shader.FindKernel("CSMain");
+        rt = new RenderTexture(256, 256, 32);
+        rt.enableRandomWrite = true;
+        rt.Create();
+        shader.SetTexture(kernelNumber, "Result", rt);
+        shader.Dispatch(kernelNumber, 256/8, 256/8, 1);
+        material.mainTexture = rt;
+    }
+}
