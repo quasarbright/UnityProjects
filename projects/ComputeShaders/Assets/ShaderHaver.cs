d7ef032f7e8d2df0f7f3e2804fbe65164ba02e65
Mike Delmonaco <mthundershock@gmail.com>
2020-01-10 00:15:37 -0500

    looks good

--- projects/ComputeShaders/Assets/ShaderHaver.cs
+++ projects/ComputeShaders/Assets/ShaderHaver.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class ShaderHaver : MonoBehaviour
 {
     public ComputeShader shader;
+    public ComputeBuffer colorBuffer;
+    public ComputeBuffer boundsBuffer;
+    [SerializeField]
+    public List<Color> colors;
+    private Color[] colorsArray;
+    public Vector2 zoomTo;
     public RenderTexture rt;
 
     public Material material;
@@ -12,6 +18,76 @@ public class ShaderHaver : MonoBehaviour
     void Start()
     {
         Debug.Log(SystemInfo.supportsComputeShaders);
+        colorsArray = new Color[256];
+        colorBuffer = new ComputeBuffer(colorsArray.Length, 4 * 4);
+        for(int i = 0; i < colorsArray.Length; i++) {
+            int listIndex = i % colors.Count;
+            colorsArray[i] = colors[listIndex];
+        }
+        colorsArray[colorsArray.Length-1] = new Color(0,0,0);
+        colorBuffer.SetData(colorsArray);
+
+        boundsBuffer = new ComputeBuffer(4, sizeof(float));
+        float xmin, xmax, ymin, ymax;
+        xmin = -2;
+        xmax = 2;
+        ymin = -2;
+        ymax = 2;
+        float[] bounds = {xmin, xmax, ymin, ymax};
+        boundsBuffer.SetData(bounds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int kernelNumber = shader.FindKernel("CSMain");
+        rt = new RenderTexture(1024, 1024, 32);
+        rt.enableRandomWrite = true;
+        rt.Create();
+        shader.SetTexture(kernelNumber, "Result", rt);
+        shader.SetBuffer(kernelNumber, "colors", colorBuffer);
+        shader.SetBuffer(kernelNumber, "bounds", boundsBuffer);
+        shader.Dispatch(kernelNumber, 1024/8, 1024/8, 1);
+        material.mainTexture = rt;
+    }
+}
+
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderHaver : MonoBehaviour
+{
+    public ComputeShader shader;
+    public ComputeBuffer colorBuffer;
+    [SerializeField]
+    public List<Color> colors;
+    private float[] Rs;
+    private float[] Gs;
+    private float[] Bs;
+    public RenderTexture rt;
+
+    public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(SystemInfo.supportsComputeShaders);
+        int numColors = 256;
+        Rs = new float[256];
+        Gs = new float[256];
+        Bs = new float[256];
+        ComputeBuffer buffer = new ComputeBuffer(, 4 * 4);
+        for(int i = 0; i < numColors; i++) {
+            int listIndex = i * colors.Count / colorsArray.Length;
+            Color color = colors[listIndex];
+            float r = color.r;
+            float g = color.g;
+            float b = color.b;
+            colorsArray[i] = colors[listIndex];
+        }
+        
     }
 
     // Update is called once per frame
@@ -22,7 +98,9 @@ public class ShaderHaver : MonoBehaviour
         rt.enableRandomWrite = true;
         rt.Create();
         shader.SetTexture(kernelNumber, "Result", rt);
+        shader.SetBuffer(kernelNumber, "colors", colorBuffer);
         shader.Dispatch(kernelNumber, 256/8, 256/8, 1);
         material.mainTexture = rt;
     }
 }
+*/
